78a2e4d05a827d5903adee54076a0cfa2db3ca6d
Kunjan Dalal <kunjee17@gmail.com>
2024-08-19 19:11:55 +0530

    tailwind and friends added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://example.com',
+  integrations: [react(), tailwind()],
   output: "server",
   adapter: netlify()
 });
