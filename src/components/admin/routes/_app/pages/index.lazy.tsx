731581fb4f4a1ea0c3c3e5bbc69dc6d543795fde
Kunjan Dalal <kunjee17@gmail.com>
2024-09-09 12:14:23 +0530

    Pages and Posts list refactored to use single component

--- src/components/admin/routes/_app/pages/index.lazy.tsx
+++ src/components/admin/routes/_app/pages/index.lazy.tsx
@@ -1,11 +1,9 @@
 import { useAsync, useMountEffect } from "@react-hookz/web";
 import { Link, createLazyFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import { Button, Card, Modal } from "react-daisyui";
+import { Loading } from "react-daisyui";
 import { deletePage, fetchPages } from "../../../../../firebase/client.ts";
-import { deleteById, removeHtmlFromMarkdown } from "../../../../../helpers";
-import { ErrorMsg } from "../../../components/errorMsg.tsx";
-import type { AnalaPage } from "../../../pages";
+import { ErrorMsg } from "../../../components";
+import { CommonList } from "../../../components/commonList.tsx";
 
 export const Route = createLazyFileRoute("/_app/pages/")({
 	component: List,
@@ -13,83 +11,43 @@ export const Route = createLazyFileRoute("/_app/pages/")({
 
 function List() {
 	const [state, action] = useAsync(() => fetchPages());
-	const [errorMsg, setErrorMsg] = useState("");
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [selectedPage, setSelectedPage] = useState<AnalaPage | undefined>(
-		undefined,
-	);
+
 	useMountEffect(action.execute);
+	if (state.status === "loading") {
+		return <Loading />;
+	}
+
+	if (state.status === "error") {
+		return <ErrorMsg errorMsg={state.error?.message} />;
+	}
+
 	return (
-		<div>
-			<Link to="/pages/add" className="btn btn-ghost normal-case">
-				{" "}
-				Add{" "}
-			</Link>
-			{errorMsg && <ErrorMsg errorMsg={errorMsg} />}
-			{state.result && (
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-					{state.result.map((page) => (
-						<Card key={page.id} className="shadow-lg">
-							<Card.Body>
-								<Card.Title>{page.name}</Card.Title>
-								<article className="prose lg:prose-xl">
-									{removeHtmlFromMarkdown(page.content)}
-								</article>
-								<div className="flex justify-between mt-4">
-									<a
-										target={"_blank"}
-										href={page.href}
-										rel="noopener noreferrer"
-									>
-										<Button color="primary">View</Button>
-									</a>
-									<Link
-										className="btn btn-secondary"
-										to="/pages/$id/edit"
-										params={{
-											id: page.id || "",
-										}}
-									>
-										Edit{" "}
-									</Link>
-									<Button
-										color={"error"}
-										onClick={() => {
-											setSelectedPage(page);
-											setIsModalOpen(true);
-										}}
-									>
-										Delete
-									</Button>
-								</div>
-							</Card.Body>
-						</Card>
-					))}
-					<Modal open={isModalOpen} responsive={true}>
-						<Modal.Header>Confirm Deletion</Modal.Header>
-						<Modal.Body>
-							Are you sure you want to delete the page "{selectedPage?.name}"?
-						</Modal.Body>
-						<Modal.Actions>
-							<Button
-								color="error"
-								onClick={async () => {
-									const res = await deleteById(selectedPage?.id, deletePage);
-									if (res._type === "Failure") {
-										setErrorMsg(res.msg || "");
-									}
-									setIsModalOpen(false);
-									setSelectedPage(undefined);
-									await action.execute();
-								}}
-							>
-								Delete
-							</Button>
-							<Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
-						</Modal.Actions>
-					</Modal>
-				</div>
-			)}
-		</div>
+		<CommonList
+			addLink={
+				<Link to="/pages/add" className="btn btn-ghost normal-case">
+					{" "}
+					Add{" "}
+				</Link>
+			}
+			reFetch={action.execute}
+			deleteItem={deletePage}
+			data={state.result?.map((item) => ({
+				id: item.id,
+				name: item.name,
+				content: item.content,
+				href: item.href,
+				editHref: (
+					<Link
+						className="btn btn-secondary"
+						to="/pages/$id/edit"
+						params={{
+							id: item.id || "",
+						}}
+					>
+						Edit{" "}
+					</Link>
+				),
+			}))}
+		/>
 	);
 }
--- src/components/admin/routes/_app/posts/index.lazy.tsx
+++ src/components/admin/routes/_app/posts/index.lazy.tsx
@@ -1,11 +1,9 @@
 import { useAsync, useMountEffect } from "@react-hookz/web";
 import { Link, createLazyFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import { Button, Card, Modal } from "react-daisyui";
+import { Loading } from "react-daisyui";
 import { deletePost, fetchPosts } from "../../../../../firebase/client.ts";
-import { deleteById, removeHtmlFromMarkdown } from "../../../../../helpers";
-import { ErrorMsg } from "../../../components/errorMsg.tsx";
-import type { Post } from "../../../posts";
+import { ErrorMsg } from "../../../components";
+import { CommonList } from "../../../components/commonList.tsx";
 
 export const Route = createLazyFileRoute("/_app/posts/")({
 	component: List,
@@ -13,82 +11,45 @@ export const Route = createLazyFileRoute("/_app/posts/")({
 
 function List() {
 	const [state, action] = useAsync(() => fetchPosts());
-	const [errorMsg, setErrorMsg] = useState("");
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [selectedPost, setSelectedPost] = useState<Post | undefined>(undefined);
+
 	useMountEffect(action.execute);
+
+	if (state.status === "loading") {
+		return <Loading />;
+	}
+
+	if (state.status === "error") {
+		return <ErrorMsg errorMsg={state.error?.message} />;
+	}
+
 	return (
-		<div>
-			<Link to="/posts/add" className="btn btn-ghost normal-case">
-				{" "}
-				Add{" "}
-			</Link>
-			{errorMsg && <ErrorMsg errorMsg={errorMsg} />}
-			{state.result && (
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-					{state.result.map((post) => (
-						<Card key={post.id} className="shadow-lg">
-							<Card.Body>
-								<Card.Title>{post.title}</Card.Title>
-								<article className="prose lg:prose-xl">
-									{removeHtmlFromMarkdown(post.content)}
-								</article>
-								<div className="flex justify-between mt-4">
-									<a
-										target={"_blank"}
-										href={post.url}
-										rel="noopener noreferrer"
-									>
-										<Button color="primary">View</Button>
-									</a>
-									<Link
-										className="btn btn-secondary"
-										to="/posts/$id/edit"
-										params={{
-											id: post.id || "",
-										}}
-									>
-										{" "}
-										Edit{" "}
-									</Link>
-									<Button
-										color={"error"}
-										onClick={() => {
-											setSelectedPost(post);
-											setIsModalOpen(true);
-										}}
-									>
-										Delete
-									</Button>
-								</div>
-							</Card.Body>
-						</Card>
-					))}
-					<Modal open={isModalOpen} responsive={true}>
-						<Modal.Header>Confirm Deletion</Modal.Header>
-						<Modal.Body>
-							Are you sure you want to delete the post "{selectedPost?.title}"?
-						</Modal.Body>
-						<Modal.Actions>
-							<Button
-								color="error"
-								onClick={async () => {
-									const res = await deleteById(selectedPost?.id, deletePost);
-									if (res._type === "Failure") {
-										setErrorMsg(res.msg || "");
-									}
-									setIsModalOpen(false);
-									setSelectedPost(undefined);
-									await action.execute();
-								}}
-							>
-								Delete
-							</Button>
-							<Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
-						</Modal.Actions>
-					</Modal>
-				</div>
-			)}
-		</div>
+		<CommonList
+			addLink={
+				<Link to="/posts/add" className="btn btn-ghost normal-case">
+					{" "}
+					Add{" "}
+				</Link>
+			}
+			reFetch={action.execute}
+			deleteItem={deletePost}
+			data={state.result?.map((item) => ({
+				id: item.id,
+				name: item.title,
+				content: item.content,
+				href: item.url,
+				editHref: (
+					<Link
+						className="btn btn-secondary"
+						to="/posts/$id/edit"
+						params={{
+							id: item.id || "",
+						}}
+					>
+						{" "}
+						Edit{" "}
+					</Link>
+				),
+			}))}
+		/>
 	);
 }
